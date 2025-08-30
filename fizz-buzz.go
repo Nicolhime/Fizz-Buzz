82a174df37c6ccc0a6dee945e8a96b136d4c8dc3
Royce Doughty <117875050+Nicolhime@users.noreply.github.com>
2025-08-29 19:55:12 -0500

    removed testing code that prints value alongside fizzbuzz

--- fizz-buzz.go
+++ fizz-buzz.go
@@ -16,13 +16,10 @@ func fizzbuzz(i int) {
 	//switch case div by 3 = "fizz" div by 5 = "buzz" div by 3 & 5 = "fizzbuzz"
 	switch {
 	case (i%3 == 0) && (i%5 == 0):
-		fmt.Print("(", i, ")")
 		fmt.Println("FizzBuzz")
 	case i%3 == 0:
-		fmt.Print("(", i, ")")
 		fmt.Println("Fizz")
 	case i%5 == 0:
-		fmt.Print("(", i, ")")
 		fmt.Println("Buzz")
 	default:
 		fmt.Println(i)
